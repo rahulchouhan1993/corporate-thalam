36952a52045a49ab941f5d35410674a685ca973c
Ganesh <ganeshwab@gmail.com>
2025-06-22 23:31:16 +0530

    design new page

--- resources/js/Pages/Delivery/Startup.jsx
+++ resources/js/Pages/Delivery/Startup.jsx
@@ -1,8 +1,88 @@
 export default function Startup(){
     return (
         <>
-        <br/><br/><br/>
-        Ye page aapko banana h
+        <section className="herobanner-services startupbanner" style={{backgroundImage: "url('/images/imag-bg.png')"}}>
+            <div className="container">
+                <h1>ARE YOU A SOCIAL MEDIA <span>INFLUENCER</span>?</h1>
+            </div>
+        </section>
+
+        <section className="focusonlysec">
+            <div className="container">
+                <div className="focusheading">
+                    <h3 className="startsmheading">Join <span>CT25</span> and Focus Only on What You Do Best</h3>
+                    <p>Unlock hassle-free, paid brand collaborations without worrying about Travel, ad bookings, Commercials or backend stress.</p>
+                </div>
+                <div className="focusdatarow">
+                    <ul className="focusdatalist">
+                        <li>Brand Deals Made Easy</li>
+                        <li>No Negotiation Stress</li>
+                        <li>Privacy & Security</li>
+                        <li>100% Advance Payments</li>
+                    </ul>
+                    <ul className="focusdatalist">
+                        <li>One Profile, Many Projects</li>
+                        <li>Free Creative Support</li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+
+        <section className="watherstartup">
+            <div className="container">
+                <h3 className="startsmheading">Whether you're a <span>nano, micro</span>, or <span>macro influencer</span>, CT25 gives you a structured and safe space to grow with real brand projects.</h3>
+                <a href="#" className="btn whiteborderbtn">Join Us</a>
+            </div>
+        </section>
+
+        <section className="handelbackendsec">
+            <div className="handeltopsec">
+                <div className="container">
+                    <h3 className="startsmheading">Let <span>CT25</span> handle the backend. You shine in the front.</h3>
+                    <div className="startuphandelrow">
+                        <ul className="handlebacklist leftlistbox">
+                            <li><span>Member Registration</span></li>
+                            <li><span>CT25 - Verfication & Confirmation</span></li>
+                            <li><span>CT25 - Initiate Welcome Board Payment</span></li>
+                            <li><span>Get Paid By CT25</span></li>
+                            <li><span>Choose Niches</span></li>
+                            <li><span>Welcome OnBoard Publish in CT25 Pages</span></li>
+                            <li>
+                                <div className="handelbackimg">
+                                    <img className="img-fluid" src="/images/handelimg.jpg" alt="Image" />
+                                </div>
+                            </li>
+                        </ul>
+                        <ul className="handlebacklist midlistbox">
+                            <li><span>Personal Branding Meta Campaign</span></li>
+                            <li>
+                                <div className="handelbackimg">
+                                    <img className="img-fluid" src="/images/handelimg.jpg" alt="Image" />
+                                </div>
+                            </li>
+                        </ul>
+                        <ul className="handlebacklist rightlistbox">
+                            <li><span>Ad Booking Confirmation</span></li>
+                            <li><span>CT25 Informs Brands Interested to Collab with you</span></li>
+                            <li><span>Filtered & Good Prospects</span></li>
+                            <li><span>On Selected Niches</span></li>
+                            <li><span>CT25 - Prepare Story Board</span></li>
+                            <li><span>CT25 - Writes Content</span></li>
+                            <li><span>CT25 - Manage Shooting with all equipments required</span></li>
+                            <li><span>CT25 - Manage Post Production</span></li>
+                            <li><span>CT25 - Manage Publish & Collab Activity</span></li>
+                            <li><span>Release Ad Payment</span></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div className="handelbottombox">
+                <div className="container">
+                    <h4>Unlock the opportunities to collob with multinational corporates</h4>
+                </div>
+            </div>
+        </section>
+
         </>
     )
 }
